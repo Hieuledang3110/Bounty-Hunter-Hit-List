7622fef789db3f620853a192b2a471ed40581c89
JohnDoe102 <89111612+Hieuledang3110@users.noreply.github.com>
2022-02-25 18:10:48 +1100

    Add files via upload

---
+++ Bounty Hunter Hit List/index.js
@@ -0,0 +1,12 @@
+let name = "";
+const hitID = "hit";
+const difficulty = "diff";
+const value = "value";
+const priority = "prio";
+
+  function ReadTargetData() {
+    let name = document.getElementById("name-input").value;
+    return name;
+    
+  }
+document.getElementById("text").innerHTML = value;
